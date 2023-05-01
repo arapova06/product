1418b57a4cf641ce8896eb0d9b982ed71fd3d585
arapova06 <arapovaajko@gmail.com>
2023-05-01 17:34:52 +0600

    first commit

---
+++ Script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+navigation: {
+        nextE1: ".swiper-button-next",
+        prevE1: ".swiper-button-prev",
+},
+    pagination:{
+        el: ".swiper-pagination"
+    },
+    mousewhe11: true,
+    keyboard: true,
+});
